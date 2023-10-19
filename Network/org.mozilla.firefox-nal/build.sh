71b6d19e2a0a61c14616dbeb30f37f4092382ca8
Shiroko <hhx.xxm@gmail.com>
2023-10-19 16:14:14 +0800

    change: improve firefox-nal

--- Network/org.mozilla.firefox-nal/build.sh
+++ Network/org.mozilla.firefox-nal/build.sh
@@ -1,92 +1,32 @@
 #!/bin/bash
-## This is a ugly version of script. Try to use it correctly.
-## We should firstly confirm the repo info is the latest
-
-# while true;do
-# sudo apt update
-# if [ "$?" = "0" ];then
-# break
-# else
-# sleep 10
-# echo "aptss is locked. Will try it later in 10 secs"
-# fi
-# done
-
-###### The check update function is diabled since we does not use UOS in building system. 
-### Some function is still ugly but i'm lazy to fix it. 
-
-VERSION_FIREFOX_IN_MOZILLA=`curl -fI 'https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=zh-CN' | grep -o 'firefox-[0-9.]\+[0-9]' | sed 's/.*firefox-//'`
-## Here we give the value to version
-version=$VERSION_FIREFOX_IN_MOZILLA
 
 export PACKAGE="org.mozilla.firefox-nal"
 export NAME="Firefox国际版"
-export VERSION="$version"
+export VERSION=""
 export ARCH="amd64"
-export URL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=zh-CN"
+export URL="fflatest.tar.bz2::https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=zh-CN"
 # autostart,notification,trayicon,clipboard,account,bluetooth,camera,audio_record,installed_apps
 export REQUIRED_PERMISSIONS=""
 
-function build(){
+export DESC1="No shady privacy policies or back doors for advertisers. Just a lightning fast browser that doesn't sell you out."
+export SECTION="web"
 
-### Write desktop
-cat << EOFFFFFF > $APP_DIR/entries/applications/org.mozilla.firefox-nal.desktop
-[Desktop Entry]
-Name=Firefox
-Name[zh_CN]=Firefox火狐浏览器
-Version=1.0
-Exec=/opt/apps/org.mozilla.firefox-nal/files/firefox/firefox %U
-Comment=Firefox
-Icon=org.mozilla.firefox-nal
-Type=Application
-Terminal=false
-StartupNotify=true
-Encoding=UTF-8
-Categories=Network;WebBrowser;
-MimeType=text/html;text/xml;application/xhtml_xml;application/x-mimearchive;x-scheme-handler/http;x-scheme-handler/https;
-
-
-EOFFFFFF
-
-### Download package
-Download_Name="fflatest.tar.bz2"
-Download_Url="https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=zh-CN"
-aria2c -c -x 16 -s 16  -d "$APP_DIR/files" "$Download_Url" -o "$Download_Name" --summary-interval=1
-tar -xvf "$APP_DIR/files"/"$Download_Name" -C "$APP_DIR/files"
-rm "$APP_DIR/files"/"$Download_Name"
-
-mkdir -p $APP_DIR/files/firefox/distribution
-### Write distribution info and policies
-cat << EOFFFFFF > $APP_DIR/files/firefox/distribution/distribution.ini
-[Global]
-id=flamescion
-version=1.0
-about=Mozilla Firefox for Spark Store
-
-[Preferences]
-app.distributor="Project Spark"
-app.distributor.channel="Spark Store"
-mozilla.partner.id="flamescion"
-EOFFFFFF
-
-cat << EOFFFFFF > $APP_DIR/files/firefox/distribution/policies.json
-{
-    "policies":
-    {
-        "DisableAppUpdate": true
-    }
-}
-EOFFFFFF
-
-
-cp $APP_DIR/files/firefox/browser/chrome/icons/default/default128.png $APP_DIR/entries/icons/org.mozilla.firefox-nal.png
-
-
+function prepare() {
+    # Inside this function. We could prepare any meta info that is needed for shirodeb.
+    # Example, fetch the version info from upstream instead manually specifying.
+    export VERSION=$(curl -sfI 'https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=zh-CN' | grep -o 'firefox-[0-9.]\+[0-9]' | sed 's/.*firefox-//')
 }
 
+function build() {
+    cp -r $SRC_DIR/${UNARCHIVED_SRC_DIRS[0]}/* $APP_DIR/files/
 
+    mkdir -p $APP_DIR/entries/applications
+    mkdir -p $APP_DIR/entries/icons/
+    mkdir -p $APP_DIR/files/firefox/distribution
 
-
-
-
-
+    cp $ROOT_DIR/templates/org.mozilla.firefox-nal.desktop $APP_DIR/entries/applications/org.mozilla.firefox-nal.desktop
+    cp $ROOT_DIR/templates/distribution.ini $APP_DIR/files/firefox/distribution/distribution.ini
+    cp $ROOT_DIR/templates/policies.json $APP_DIR/files/firefox/distribution/policies.json
+    utils.icon.collect "$APP_DIR/files/firefox/browser/chrome/icons/default/"
+    cp $APP_DIR/files/firefox/browser/chrome/icons/default/default128.png $APP_DIR/entries/icons/org.mozilla.firefox-nal.png
+}
